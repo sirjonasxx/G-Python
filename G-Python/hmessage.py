b2033beef9fde19cf489537781b11b51c9c47a9a
sirjonasxx <36828922+sirjonasxx@users.noreply.github.com>
2020-10-04 20:49:51 +0200

    implement HMessage

--- G-Python/hmessage.py
+++ G-Python/hmessage.py
@@ -1,17 +1,35 @@
 import sys
+from enum import Enum
 
-from gextension import Extension
+from hpacket import HPacket
 
-extension_info = {
-    "title": "G-Python",
-    "description": "Test python extension",
-    "version": "1.0",
-    "author": "sirjonasxx"
-}
-
-extension = Extension(extension_info, sys.argv, None)
-
+class Direction(Enum):
+    TO_SERVER = 0
+    TO_CLIENT = 1
 
 class HMessage:
-    def __init__(self):
-        pass
+    def __init__(self, packet, direction, index, is_blocked=False):
+        self.packet = packet
+        self.direction = direction
+        self.index = index
+        self.is_blocked = is_blocked
+
+    @classmethod
+    def reconstruct_from_java(cls, extension, string):
+        obj = cls.__new__(cls)
+        super(HMessage, obj).__init__()
+
+        split = string.split(' ', 3)
+        obj.is_blocked = split[0] == '1'
+        obj.index = int(split[1])
+        obj.direction = Direction.TO_CLIENT if split[2] == 'TOCLIENT' else Direction.TO_SERVER
+        obj.packet = HPacket.reconstruct_from_java(extension, split[3])
+        return obj
+
+    def __repr__(self):
+        return "{}\t{}\t{}\t{}".format(
+            '1' if self.is_blocked else '0',
+            self.index,
+            'TOCLIENT' if self.direction == Direction.TO_CLIENT else 'TOSERVER',
+            repr(self.packet)
+        )
