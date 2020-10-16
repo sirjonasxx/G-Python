007da9ae68611d3f5020d881d6483b811b3dfc3b
sirjonasxx <36828922+sirjonasxx@users.noreply.github.com>
2020-10-16 03:31:15 +0200

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="g-python",
-    version="0.0.2",
+    version="0.0.3",
     author="sirjonasxx",
     author_email="sirjonasxx@hotmail.com",
     description="G-Earth extension interface for Python.",
