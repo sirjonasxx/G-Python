69de2ee35d3606da4bcab66c591aa8cb0e4f5520
César <cvxz@pm.me>
2021-01-09 23:47:12 -0300

    UnityRoomFurni class added

---
+++ room_stuff.py
@@ -0,0 +1,35 @@
+import sys
+
+from g_python.gextension import Extension
+from g_python.hunitytools import UnityRoomUsers, UnityRoomFurni
+
+extension_info = {
+    "title": "Room stuff",
+    "description": "g_python test",
+    "version": "1.2",
+    "author": "sirjonasxx & Hellsin6"
+}
+
+ext = Extension(extension_info, sys.argv)
+ext.start()
+
+def print_furnis(furnis):
+	for furni in furnis:
+		print("furno",furni.id, furni.type_id, furni.tile, furni.owner)
+
+room_users = UnityRoomUsers(ext)
+room_users.on_new_users(lambda users: print(list(map(str, users))))
+
+room_furni = RoomFurni(ext)
+room_furni.on_floor_furni_load(lambda furnis: print_furnis(furnis))
+
+'''
+LEGACY FLASH STUFF
+
+room_furni.on_wall_furni_load(lambda furni: print("Found {} wall furniture in room".format(len(furni))))
+
+# current room users & furniture are always available under:
+# room_furni.floor_furni        (list of HFloorItem)
+# room_furni.wall_furni         (list of HWallItem)
+# room_users.room_users         (list of HEntitity)
+'''
