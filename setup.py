80909249991277e9c42d15608c61b5b00db9eeb7
sirjonasxx <36828922+sirjonasxx@users.noreply.github.com>
2021-04-07 05:00:42 +0200

    Revert "we're jumping in time"
    This reverts commit aa1da9817ecc7ea225308876c1bc97cfc493e1ec.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="g-python",
-    version="1.3.0",
+    version="0.1.3",
     author="sirjonasxx",
     author_email="sirjonasxx@hotmail.com",
     description="G-Earth extension interface for Python.",
