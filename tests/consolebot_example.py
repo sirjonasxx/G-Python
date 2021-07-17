703a0e439686e04c72e81fbb2bd89e5dfdc8ed61
denio4321 <danimartinezortin@gmail.com>
2021-07-17 04:07:28 +0200

    Add a tool to make extensions easier to develop without UI.

---
+++ tests/consolebot_example.py
@@ -0,0 +1,32 @@
+import sys
+
+from g_python.gextension import Extension
+from g_python_bot import Bot
+
+extension_info = {
+    "title": "ConsoleBot",
+    "description": "Make extension ez without ui",
+    "version": "1.0",
+    "author": "denio4321"
+}
+
+ext = Extension(extension_info, sys.argv)
+ext.start()
+
+
+def pong():
+    console_bot.send_message("pong")
+
+def ping():
+    console_bot.send_message("ping")
+
+# Creates the object
+console_bot = Bot(ext, botname="ConsoleBot-Example")
+# Defines the command and the call functions
+console_bot.on_command(':ping', pong)
+console_bot.on_command(':pong', ping)
+
+# Runs the bot
+console_bot.start()
+# Sends a welcome message once connected
+console_bot.send_message("Hi! Nice to meet you! This is a example bot.")
